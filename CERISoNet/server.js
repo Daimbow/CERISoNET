38b71824f6860303a9388efebd5638c53d026179
JulienJAMME-ceri <julien.jamme@alumni.univ-avignon.fr>
2025-03-05 22:14:51 +0100

    config d'origine avant git

---
+++ CERISoNet/server.js
@@ -0,0 +1,73 @@
+
+// JULIEN JAMME ILSEN
+
+import express from 'express';
+import fs from 'fs';
+import https from 'https';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import pg from 'pg'; 
+
+// Chemins des fichiers et dossiers
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Connexion à PostgreSQL
+const pool = new pg.Pool({
+    user: 'uapv2500814',
+    host: 'pedago01c.univ-avignon.fr',
+    database: 'etd',
+    password: 'ALCrxZ',
+    port: 5432
+});
+
+// Instance de l'application Express
+const app = express();
+
+// Middlware pour les fichiers statiques
+app.use(express.static(path.join(__dirname, 'public_html')));
+
+// Configuration du serveur HTTPS.
+const options = {
+    pfx: fs.readFileSync(path.join(__dirname, 'certificat.pfx')),
+    passphrase: 'NodeTP',
+};
+
+// Création du serveur HTTPS.
+https.createServer(options, app).listen(3223, () => {
+    console.log('Serveur HTTPS démarré sur https://pedago.univ-avignon.fr:3223');
+});
+
+// Middleware qui définit la route principal.
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'CERISoNet/dist/ceriso-net/browser/index.html'));
+});
+
+// Middleware pour parser les requêtes JSON
+app.use(express.json());
+
+// Middleware qui définit la route pour récupérer les informations de connexions.
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    if(!username || !password){
+        return res.status(400).send({message: "Veuillez fournir un nom d'utilisateur et un mot de passe."});
+    }
+
+    try{
+        const result = await pool.query(
+            'SELECT * FROM fredouil.users WHERE identifiant = $1 AND motpasse = $2',
+            [username, password]
+        );
+
+        if(result.rowCount > 0){
+            res.status(200).json({message : 'Connexion réussie', user : result.rows[0]})
+
+        }else{
+            return res.status(400).send({message: "Identifiants incorrects."});
+        }
+    }catch (error) {
+        console.error('Erreur lors de la connexion :', error);
+        res.status(500).json({ message: 'Erreur serveur' });
+    }
+});
--- CERISoNet/src/app/app.component.ts
+++ CERISoNet/src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
+    standalone: true,
+    imports: [RouterOutlet, ReactiveFormsModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
+  })
 export class AppComponent {
-  title = 'CERISoNet';
+    loginForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+        this.loginForm = this.fb.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+    }
+
+    onSubmit() {
+        if (this.loginForm.valid) {
+            console.log(this.loginForm.value);
+        } else {
+            console.log("Formulaire non valide.");
+        }
+    }
 }
