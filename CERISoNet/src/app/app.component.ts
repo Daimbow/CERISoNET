ab2288e51291d9cfe32d7de76449400bf3530386
JulienJAMME-ceri <julien.jamme@alumni.univ-avignon.fr>
2025-03-06 12:51:10 +0100

    t

--- CERISoNet/src/app/app.component.ts
+++ CERISoNet/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap'; // Import ng-bootstrap
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap'; // Import du module alert
+import { CommonModule } from '@angular/common'; // Import de CommonModule pour les directives Angular
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgbAlertModule], // Ajout du module d'alert
+  imports: [CommonModule, NgbAlertModule], // Ajout du module ng-bootstrap et CommonModule
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
