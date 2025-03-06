2d072bdca5205eac94e6941a5b5eaeccc880b24b
JulienJAMME-ceri <julien.jamme@alumni.univ-avignon.fr>
2025-03-06 12:46:17 +0100

    test boost

--- CERISoNet/src/app/app.component.ts
+++ CERISoNet/src/app/app.component.ts
@@ -1,30 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap'; // Import ng-bootstrap
 
 @Component({
   selector: 'app-root',
-    standalone: true,
-    imports: [RouterOutlet, ReactiveFormsModule],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
-  })
+  standalone: true,
+  imports: [NgbAlertModule], // Ajout du module d'alert
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
 export class AppComponent {
-    loginForm: FormGroup;
-
-    constructor(private fb: FormBuilder) {
-        this.loginForm = this.fb.group({
-            username: ['', Validators.required],
-            password: ['', Validators.required]
-        });
-    }
-
-    onSubmit() {
-        if (this.loginForm.valid) {
-            console.log(this.loginForm.value);
-        } else {
-            console.log("Formulaire non valide.");
-        }
-    }
+  // Liste des types d'alertes à afficher
+  alertTypes = ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark'];
 }
