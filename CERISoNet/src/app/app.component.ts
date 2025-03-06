a6e15a60524cf9b79bd0eb72ad5a7665acb3c6f6
JulienJAMME-ceri <julien.jamme@alumni.univ-avignon.fr>
2025-03-06 16:41:13 +0100

    test nt

--- CERISoNet/src/app/app.component.ts
+++ CERISoNet/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap'; // Import du module alert
 import { CommonModule } from '@angular/common'; // Import de CommonModule pour les directives Angular
+import { RouterModule } from '@angular/router'; // Import de RouterModule pour le routage
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, NgbAlertModule], // Ajout du module ng-bootstrap et CommonModule
+  imports: [CommonModule, NgbAlertModule, RouterModule], // Ajout de RouterModule pour les routes
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
