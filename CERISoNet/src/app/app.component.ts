4ef070035862d536ab945a01d0ee3af5f6ac0688
JulienJAMME-ceri <julien.jamme@alumni.univ-avignon.fr>
2025-03-11 18:56:57 +0100

    test notification

--- CERISoNet/src/app/app.component.ts
+++ CERISoNet/src/app/app.component.ts
@@ -1,18 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbAlertModule, NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, NgbAlertModule, RouterModule, ReactiveFormsModule, HttpClientModule],
+  imports: [CommonModule, NgbAlertModule, RouterModule, ReactiveFormsModule, HttpClientModule, NgbToastModule ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+    showToast: boolean = false;
+    toastMessage: string = '';
+    toastType: string = '';
+
+    getToastClass(): string {
+        switch (this.toastType) {
+          case 'success':
+            return 'text-bg-success'; 
+          case 'error':
+            return 'text-bg-danger'; 
+          default:
+            return '';
+        }
+      }
+
+    showToastMessage(message: string, type: string): void {
+        this.toastMessage = message;
+        this.toastType = type;
+        this.showToast = true;
+    
+        setTimeout(() => {
+          this.showToast = false;
+        }, 3000);  
+      }
+
+
   loginForm: FormGroup;
 
   constructor(private fb: FormBuilder, private http: HttpClient) {
@@ -22,22 +49,23 @@ export class AppComponent {
     });
   }
 
-  async onSubmit() {
 
+  async onSubmit() {
     if (this.loginForm.valid) {
       const formData = this.loginForm.value;
-      console.log('Formulaire soumis', formData);  
 
       // Création des en-têtes pour la requête
       const headers = new HttpHeaders().set('Content-Type', 'application/json');
 
       try {
-
         const response = await this.http.post('https://pedago.univ-avignon.fr:3223/login', formData, { headers }).toPromise();
         console.log('Réponse du serveur:', response);
+
+        this.showToastMessage('Connexion réussie!', 'success');
       } catch (error) {
         console.error('Erreur:', error);
-        alert('Erreur lors de la connexion. Vérifiez vos informations et réessayez.');
+
+        this.showToastMessage('Erreur lors de la connexion. Vérifiez vos informations et réessayez.', 'error');
       }
     } else {
       console.log('Le formulaire est invalide');
